daffae0391b2d4163adbe5f4acd1e77919df1d55
Billy Chan <shing6326@gmail.com>
2023-12-23 11:18:57 +0800

    :art: big update

--- custom_components/zb-ir01-to-climate/climate.py
+++ custom_components/zb-ir01-to-climate/climate.py
@@ -33,14 +33,13 @@ code = {
         "31": "8601020f0000",
         "32": "860102100095"
     },
-    "poweron": "860100000087",
-    "poweroff": "860100010086",
     "mode": {
         "auto": "860101000086",
         "cool": "860101010087",
         "dry": "860101020084",
         "fan_only": "860101030085",
         "heat": "860101040082"
+        "off": "860100010086",
     },
     "fan_speed": {
         "auto": "860104000083",
@@ -65,7 +64,13 @@ class ZBACClimateEntity(ClimateEntity):
         self._name = climate_name
         self.entity_id = climate_id or None
         self._attr_temperature_unit = TEMP_CELSIUS
-        self._state = {}
+
+        self._current_temperature = "0"
+        self._target_temperature = "0"
+        self._fan_mode = "auto"
+        self._hvac_mode = HVAC_MODE_OFF
+        self._last_command = ""
+        self._last_received_command = ""
 
         # Subscribe to changes in the sensor
         self._sensor_unsub = async_track_state_change(
@@ -75,9 +80,8 @@ class ZBACClimateEntity(ClimateEntity):
     async def async_sensor_state_listener(self, entity_id, old_state, new_state):
         if new_state is None:
             return
-        parsed_state = self.parse_sensor_data(new_state.state)
-        if parsed_state:
-            self._state = parsed_state
+        self._last_received_command = new_state.state
+        if self.parse_sensor_data(new_state.state):
             self.async_write_ha_state()  # Update the state in Home Assistant
 
     @property
@@ -86,18 +90,7 @@ class ZBACClimateEntity(ClimateEntity):
 
     @property
     def hvac_mode(self):
-        if self._state.get('switch', '') == '01':
-            return HVAC_MODE_OFF
-        elif self._state.get('mode', '') == '00':
-            return HVAC_MODE_AUTO
-        elif self._state.get('mode', '') == '01':
-            return HVAC_MODE_COOL
-        elif self._state.get('mode', '') == '02':
-            return HVAC_MODE_DRY
-        elif self._state.get('mode', '') == '03':
-            return HVAC_MODE_FAN_ONLY
-        elif self._state.get('mode', '') == '04':
-            return HVAC_MODE_HEAT
+        return self._hvac_mode
 
     @property
     def hvac_modes(self):
@@ -105,11 +98,11 @@ class ZBACClimateEntity(ClimateEntity):
 
     @property
     def current_temperature(self):
-        return self._state.get('temperature', 0)
+        return self._current_temperature
 
     @property
     def target_temperature(self):
-        return self._state.get('temperature', 0)
+        return self._target_temperature
 
     @property
     def target_temperature_step(self):
@@ -117,7 +110,7 @@ class ZBACClimateEntity(ClimateEntity):
 
     @property
     def fan_mode(self):
-        return self._state.get('air_volume', 'auto')
+        return self._fam_mode
 
     @property
     def fan_modes(self):
@@ -146,27 +139,41 @@ class ZBACClimateEntity(ClimateEntity):
 
     def parse_sensor_data(self, data):
         if data[0:2] != "08":
-            return {}
-        if not self.verify_checksum(data):
-            _LOGGER.error(f"Invalid checksum of data '{data}'")
-            return {}
+            return False
         try:
+            if not self.verify_checksum(data):
+                raise ValueError(f"Invalid checksum of data '{data}'")
             switch = data[2:4]
             mode = data[4:6]
             # special handle of fan_only signal, the toshiba remote returns 08ff000603f2 when in fan mode
             if switch == "ff" and mode == "00":
                 switch = "00"
                 mode = "03"
+            # validate parsed value
+            if switch != "00" and switch != "01" or int(temperature) < 16 or int(temperature) > 32:
+                raise ValueError("Invalid on/off or temperature value")
             # Check if the temperature data slice is correct
             temp_hex = data[6:8]
             temperature = int(temp_hex, 16) + 16
-            air_volume = ["auto", "low", "medium", "high"][int(data[8:10], 16)]
-            if switch != "00" and switch != "01" or int(temperature) < 16 or int(temperature) > 32:
-                raise ValueError("Invalid on/off or temperature value")
-            return {"switch": switch, "mode": mode, "temperature": temperature, "air_volume": air_volume}
+            # Set temperature and fan mode
+            self._current_temperature = temperature
+            self._target_temperature = temperature
+            self._fan_mode = ["auto", "low", "medium", "high"][int(data[8:10], 16)]
+            # Set HVAC mode
+            if switch == '01':
+                self._hvac_mode = HVAC_MODE_OFF
+            else:
+                self._hvac_mode = {
+                    '00': HVAC_MODE_AUTO,
+                    '01': HVAC_MODE_COOL,
+                    '02': HVAC_MODE_DRY,
+                    '03': HVAC_MODE_FAN_ONLY,
+                    '04': HVAC_MODE_HEAT
+                }.get(mode, HVAC_MODE_OFF)
+            return True
         except ValueError as e:
             _LOGGER.error(f"Error parsing sensor data '{data}': {e}")
-            return {}
+            return False
 
     async def send_command(self, command):
         """ Helper function to send command to the climate device. """
@@ -177,7 +184,7 @@ class ZBACClimateEntity(ClimateEntity):
                 "value": '"'+command+'"'
             }
         )
-        self._state['last_command'] = command  # You might want to store more specific state
+        self._last_command = command  # You might want to store more specific state
         self.async_write_ha_state()  # Inform Home Assistant of state change
     
     async def async_set_temperature(self, **kwargs):
@@ -185,22 +192,26 @@ class ZBACClimateEntity(ClimateEntity):
         if temperature is not None:
             hex_code = code['temperature'].get(str(int(temperature)), None)
             if hex_code:
+                self._current_temperature = temperature
+                self._target_temperature = temperature
                 await self.send_command(hex_code)
     
     async def async_set_hvac_mode(self, hvac_mode):
         hex_code = code['mode'].get(hvac_mode, None)
         if hex_code:
+            self._hvac_mode = hvac_mode
             await self.send_command(hex_code)
     
     async def async_turn_on(self):
-        await self.send_command(code['poweron'])
+        await async_set_hvac_mode(HVAC_MODE_AUTO)
     
     async def async_turn_off(self):
-        await self.send_command(code['poweroff'])
+        await async_set_hvac_mode(HVAC_MODE_OFF)
     
     async def async_set_fan_mode(self, fan_mode):
         hex_code = code['fan_speed'].get(fan_mode, None)
         if hex_code:
+            self._fan_mode = fan_mode
             await self.send_command(hex_code)
     
     async def async_will_remove_from_hass(self):
